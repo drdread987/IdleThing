49469b068ec779b32c9f3d074a6e006d62f3c113
JoshuaShequin <joshua.shequin@gmail.com>
2019-10-30 15:43:39 -0700

    Started more formal work on the event handler

--- Bases/ObjectEventHandler.py
+++ Bases/ObjectEventHandler.py
@@ -1,16 +1,33 @@
 
 class ObjectEventHandler:
 
-    def __init__(self):
+    def __init__(self, obj_handler):
 
         self.listeners = {}  # {event-type: {publisher: {caller: [function to call, *args]}}}
+        self.parent = obj_handler
 
-    def add_listener(self, event_key, func, *args):
+    def add_listener(self, event_key, source, callback, lifespan, *args):
+        """
+        :param event_key: the key for the event, view the event docs for which key to use.
+        :param source: The object that is the subscriber of the event.
+        :param callback: The function called upon this events activation.
+        :param lifespan: How many times this event is called before the subscriber is removed
+                          from the list. 0 is infinite or until the subscriber object is dead.
+        :param args: Any arguments the subscriber wants passed to the callback function.
+        :return: None
+        :return:
+        """
+        if event_key in self.listeners:
+            self.listeners[event_key].append([source, callback, lifespan, args])
+        else:
+            self.listeners[event_key] = [[source, callback, lifespan, args]]
+
+    def find_subscriber_by_event(self, event_key, source):
 
         if event_key in self.listeners:
-            self.listeners[event_key].append({})
-        else:
-            self.listeners[event_key] = [[func, args]]
+            for subscriber in self.listeners[event_key]:
+                if subscriber[0] == source:
+                    return subscriber
 
     def call_listeners(self, event_key):
 
@@ -20,3 +37,14 @@ class ObjectEventHandler:
                     listener[0](listener[1])
 
             del self.listeners[event_key]
+
+    def listen_friendly_unit_takes_damage(self, source, callback, lifespan, *args):
+        """
+        :param source: The object that is the subscriber of the event.
+        :param callback: The function called upon this events activation.
+        :param lifespan: How many times this event is called before the subscriber is removed
+                          from the list. 0 is infinite or until the subscriber object is dead.
+        :param args: Any arguments the subscriber wants passed to the callback function.
+        :return: None
+        """
+        pass
