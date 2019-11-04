85b199a6a38d7939e9ec22cdc8e416486cea2107
JoshuaShequin <joshua.shequin@gmail.com>
2019-11-04 10:41:17 -0800

    Working on events.

--- Bases/ObjectEventHandler.py
+++ Bases/ObjectEventHandler.py
@@ -2,7 +2,11 @@
 class ObjectEventHandler:
 
     def __init__(self, obj_handler):
-
+        """
+        Constructor for our Object Event Handler. Creates our dictionary of events.
+        :param obj_handler: The parent obj handler that the event handler will use to check if
+                            objects are alive.
+        """
         self.listeners = {}  # {event-type: {publisher: {caller: [function to call, *args]}}}
         self.parent = obj_handler
 
@@ -15,7 +19,7 @@ class ObjectEventHandler:
                           from the list. 0 is infinite or until the subscriber object is dead.
         :param args: Any arguments the subscriber wants passed to the callback function.
         :return: None
-        :return:
+        :return: None
         """
         if event_key in self.listeners:
             self.listeners[event_key].append([source, callback, lifespan, args])
@@ -29,22 +33,15 @@ class ObjectEventHandler:
                 if subscriber[0] == source:
                     return subscriber
 
-    def call_listeners(self, event_key):
-
-        if event_key in self.listeners:
-            for event in self.listeners:
-                for listener in self.listeners[event]:
-                    listener[0](listener[1])
-
-            del self.listeners[event_key]
-
-    def listen_friendly_unit_takes_damage(self, source, callback, lifespan, *args):
+    def event_friendly_unit_takes_damage(self, source, target, amount, dmg_type, spell_id):
         """
-        :param source: The object that is the subscriber of the event.
-        :param callback: The function called upon this events activation.
-        :param lifespan: How many times this event is called before the subscriber is removed
-                          from the list. 0 is infinite or until the subscriber object is dead.
-        :param args: Any arguments the subscriber wants passed to the callback function.
+        Event ID 0, Happens when a friendly unit takes damage.
+        :param source: Source of the damage
+        :param target: The unit that took the damage
+        :param amount: The amount of damage taken
+        :param dmg_type: the type of damage dealt
+        :param spell_id: the spell ID that created the damage
         :return: None
         """
+        # TODO
         pass
